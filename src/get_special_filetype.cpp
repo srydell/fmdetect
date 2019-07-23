e58dbe2c7aca2c9f3a64bbe1d1e6ee511d34b41c
srydell <simonwrydell@gmail.com>
2019-07-23 21:16:41 +0200

    Added command line parsing

--- src/get_special_filetype.cpp
+++ src/get_special_filetype.cpp
@@ -2,11 +2,31 @@
 #include "filetypeMatcher.h"
 #include "matchers.h"
 #include <array>
+#include <clara.hpp>
 
 int main(int argc, char **argv) {
-	std::cout << "You have entered " << argc << " arguments:"
-	          << "\n";
+	std::string preliminaryFiletype;
+	std::string path;
+	bool showHelp = false;
+	auto cli = clara::Help(showHelp) |
+	           clara::Opt(preliminaryFiletype,
+	                      "preliminaryFiletype")["-ft"]["--filetype"](
+	               "What is the current filetype? E.g. cpp") |
+	           clara::Opt(path, "path")["-p"]["--path"](
+	               "Path to the file being examined.");
 
-	for (int i = 1; i < argc; ++i)
-		std::cout << argv[i] << "\n";
+	auto result = cli.parse(clara::Args(argc, argv));
+	if (!result) {
+		std::cerr << "Error in command line: " << result.errorMessage()
+		          << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (showHelp) {
+		std::cout << cli << '\n';
+		return EXIT_SUCCESS;
+	}
+
+	std::cout << "Filetype is: " << preliminaryFiletype << '\n';
+	std::cout << "Path is: " << path << '\n';
 }
