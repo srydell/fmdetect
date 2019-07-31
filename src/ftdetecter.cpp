9fb8ad701d8b5175021b6e5707b3ec8932e2080d
srydell <simonwrydell@gmail.com>
2019-07-31 17:37:28 +0200

    Added functionality for more than one source file by separating them by ,
    * --path renamed to --paths
    
    * Added some command line tests to make sure it works

--- src/ftdetecter.cpp
+++ src/ftdetecter.cpp
@@ -1,16 +1,30 @@
 #include "filetype_matcher.h"
 #include <clara.hpp>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> parsePaths(const std::string &paths) {
+	std::vector<std::string> parsed;
+	std::string path;
+	std::istringstream tokenStream(paths);
+	while (std::getline(tokenStream, path, ',')) {
+		parsed.push_back(path);
+	}
+	return parsed.empty() ? std::vector({paths}) : parsed;
+}
 
 int main(int argc, char **argv) {
 	std::string preliminaryFiletype;
-	std::string path;
+	std::string paths;
 	bool showHelp = false;
 	auto cli = clara::Help(showHelp) |
 	           clara::Opt(preliminaryFiletype, "filetype")["-f"]["--filetype"](
 	               "What is the current filetype? E.g. cpp") |
-	           clara::Opt(path, "path")["-p"]["--path"](
-	               "Path to the file being examined.");
+	           clara::Opt(paths, "paths")["-p"]["--paths"](
+	               "Path(s) to the file being examined. May be multiple "
+	               "separated by a comma (,).");
 
 	auto result = cli.parse(clara::Args(argc, argv));
 	if (!result) {
@@ -18,13 +32,17 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	if (showHelp || path.empty() || preliminaryFiletype.empty()) {
+	if (showHelp || paths.empty() || preliminaryFiletype.empty()) {
 		std::cout << cli << '\n';
 		return EXIT_SUCCESS;
 	}
 
-	if (auto specialFiletype =
-	        SpecialFiletype::getSpecialFiletype(preliminaryFiletype, path)) {
-		std::cout << specialFiletype.value() << '\n';
+	for (const auto &path : parsePaths(paths)) {
+		if (auto specialFiletype = SpecialFiletype::getSpecialFiletype(
+		        preliminaryFiletype, path)) {
+			std::cout << specialFiletype.value() << '\n';
+			return EXIT_SUCCESS;
+		}
 	}
+	return EXIT_FAILURE;
 }
